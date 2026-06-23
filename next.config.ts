b088f51062ac39a76ad999a4648ad84cfd6e635f
karlosma0416 <1373086696@qq.com>
2026-06-23 13:28:43 +0800

    fix GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/activelearning-web" : "",
+  assetPrefix: isProd ? "/activelearning-web/" : "",
 };
 
 export default nextConfig;
